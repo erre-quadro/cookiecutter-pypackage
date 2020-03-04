f87b7c4790a2230579e9eaa4bd75e27a03d91072
Paolo Arduin <paolo.arduin@errequadrosrl.com>
2020-03-04 17:41:47 +0100

    Fix version bumping

--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -1,54 +1,50 @@
-#!/usr/bin/env python
-
-"""The setup script."""
-
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
 requirements = []
 
-setup_requirements = ['pytest-runner']
+setup_requirements = ["pytest-runner"]
 
-test_requirements = ['pytest']
+test_requirements = ["pytest"]
 
 {%- set license_classifiers = {
-    'Apache Software License 2.0': 'License :: OSI Approved :: Apache Software License',
+    "Apache Software License 2.0": "License :: OSI Approved :: Apache Software License",
 } %}
 
 setup(
     author="{{ cookiecutter.full_name.replace('\"', '\\\"') }}",
-    author_email='{{ cookiecutter.email }}',
-    python_requires='>=3.5',
+    author_email="{{ cookiecutter.email }}",
+    python_requires=">=3.6",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
 {%- if cookiecutter.select_license in license_classifiers %}
-        '{{ license_classifiers[cookiecutter.select_license] }}',
+        "{{ license_classifiers[cookiecutter.select_license] }}",
 {%- endif %}
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     description="{{ cookiecutter.project_short_description }}",
     install_requires=requirements,
 {%- if cookiecutter.select_license in license_classifiers %}
     license="{{ cookiecutter.select_license }}",
 {%- endif %}
-    long_description=readme + '\n\n' + history,
+    long_description=readme + "\n\n" + history,
     include_package_data=True,
-    keywords='{{ cookiecutter.project_slug }}',
-    name='{{ cookiecutter.project_slug }}',
-    packages=find_packages(include=['{{ cookiecutter.project_slug }}', '{{ cookiecutter.project_slug }}.*']),
+    keywords="{{ cookiecutter.project_slug }}",
+    name="{{ cookiecutter.project_slug }}",
+    packages=find_packages(include=["{{ cookiecutter.project_slug }}", "{{ cookiecutter.project_slug }}.*"]),
     setup_requires=setup_requirements,
-    test_suite='tests',
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://github.com/{{ cookiecutter.github_username }}/{{ cookiecutter.project_slug }}',
-    version='{{ cookiecutter.version }}',
+    url="https://github.com/{{ cookiecutter.github_username }}/{{ cookiecutter.project_slug }}",
+    version="{{ cookiecutter.version }}",
     zip_safe=False,
 )
