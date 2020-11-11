8585a06800a99d287e5153b140330f49575d4dd5
Paolo Arduin <paolo.arduin@errequadrosrl.com>
2020-11-11 11:06:53 +0100

    Use _about.py info in setup.py

--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup, find_packages
+from {{ cookiecutter.project_slug }} import _about
 
 with open("README.rst") as readme_file:
     readme = readme_file.read()
@@ -14,8 +15,8 @@ test_requirements = ["pytest"]
 } %}
 
 setup(
-    author="{{ cookiecutter.full_name.replace('\"', '\\\"') }}",
-    author_email="{{ cookiecutter.email }}",
+    author=_about.__author__,
+    author_email=_about.__email__,
     python_requires=">=3.6",
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
@@ -28,20 +29,20 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
     ],
-    description="{{ cookiecutter.project_short_description }}",
+    description=_about.__summary__,
     install_requires=requirements,
 {%- if cookiecutter.select_license in license_classifiers %}
-    license="{{ cookiecutter.select_license }}",
+    license="{{cookiecutter.select_license}}",
 {%- endif %}
     long_description=readme,
     include_package_data=True,
-    keywords="{{ cookiecutter.project_slug }}",
-    name="{{ cookiecutter.project_slug }}",
-    packages=find_packages(include=["{{ cookiecutter.project_slug }}", "{{ cookiecutter.project_slug }}.*"]),
+    keywords=_about.__title__,
+    name=_about.__title__,
+    packages=find_packages(include=[_about.__title__, f"{_about.__title__}.*"]),
     setup_requires=setup_requirements,
     test_suite="tests",
     tests_require=test_requirements,
-    url="https://github.com/{{ cookiecutter.github_username }}/{{ cookiecutter.project_slug }}",
-    version="{{ cookiecutter.version }}",
+    url=f"https://github.com/{{ cookiecutter.github_username }}/{_about.__title__}",
+    version=_about.__version__,
     zip_safe=False,
 )
